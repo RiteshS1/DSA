a0f2ae79b4e439886c44654fa3781db465e98ed2
Ritesh Sharma <119002315+RiteshS1@users.noreply.github.com>
2024-06-17 16:14:51 +0530

    Add files via upload

---
+++ Arrays/functionarrays.cpp
@@ -0,0 +1,106 @@
+#include<iostream>
+using namespace std;
+//-----------------------------EXAMPLE 1----------------------------------------
+// void solve(int array[], int a){
+//     array[1]=50;
+// }
+
+
+// //what happened was that array[] se paas by reference ke through arr ki value me change hoga!
+
+
+// int main()
+// {
+ 
+//  //As we cant find the exact no. of elements with any formula; arrays me functions m argument me arr ke saath saath uska size dena jruri h
+
+//     int arr[]= {10,20,30};
+//     int size=3;
+
+//     solve(arr,size);
+
+
+//     for(int i=0; i<3;i++){
+//         cout<<arr[i]<<endl;
+//     } 
+//     return 0;
+// }
+
+
+
+
+
+
+//-----------------------------EXAMPLE 2--------------------------------------------------------
+
+// void solve( int num[], int &n){
+//     for(int i=0; i<n;i++){
+//         num[i]= num[i]+10;
+//     }
+//     return;
+
+// }
+
+
+
+// void swapInt(int &a, int &b){
+//     a=a+b;
+//     b=a-b;
+//     a=a-b;
+//     cout<<a<<" "<<b;
+// }
+
+
+
+
+// void swapArr(int arr[], int i, int j){
+    
+
+
+//         int temp= arr[i];
+//         arr[i]=arr[j];
+//         arr[j]= temp;
+
+//         cout<<arr[0];
+//         cout<<arr[4];
+    
+// }
+
+int main(){
+
+    // int arr[4]= {0};
+    // int size = 4;
+
+    // solve(arr,size);
+
+
+    // for(int i=0;i<4;i++){
+    //     cout<<arr[i]<<endl;
+
+    // }
+
+    // int x=10, y=20;
+
+    // swapInt(x,y);
+
+
+
+    //swapppparrrayyyyyyyyyy
+
+    // int arr[] ={10,20,30,40,50};
+    // swapArr(arr,0,4);
+
+
+
+
+
+
+
+
+
+
+    return 0;
+}
+//output me 10 10 10 10 mill jayega
+
+
