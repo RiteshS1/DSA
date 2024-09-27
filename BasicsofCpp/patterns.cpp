c45b50bef7ee3934c6916b9a458e2d6d78a29552
Ritesh Sharma <119002315+RiteshS1@users.noreply.github.com>
2024-09-28 03:41:55 +0530

    Update patterns.cpp

--- BasicsofCpp/patterns.cpp
+++ BasicsofCpp/patterns.cpp
@@ -7,6 +7,7 @@ int main()
 
     // int side;
     // cin>>side;
+    
     // for(int i=0; i<side;i++){
     //     for (int j= 0; j<side; j++){
 
@@ -222,7 +223,15 @@ G H I J
 
     // }
 
-
+    int n; 
+    cin>>n;
+    for(int i=0; i<n; i++){
+        for(int j=0;j<i;j--){
+            cout<<"_";
+        }
+        for(int k =0; k<n-i;k--)cout<<"*";
+        cout<<endl;
+    }
 
 
 
